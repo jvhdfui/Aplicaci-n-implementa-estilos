bf62f1391528262f79ed9a02476f7ff657608101
Ana Karen López <137356105+jvhdfui@users.noreply.github.com>
2026-02-23 08:37:01 -0600

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,13 @@
+import flet as ft
+
+def main (page:ft.Page):
+    page.title = "Prueba_1"
+    page.window.min_width = 600
+    page.window.min_height = 400
+
+    texto_1 = ft.Text("Hola mundo desde Flet")
+
+    page.add(texto_1)
+
+    if __name__ == "__main__":
+        ft.run(main)
